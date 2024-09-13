9aa918b1323a7f1118f5c3d059c910922d18a0bc
森祐一朗 <apotikisin4632@gmail.com>
2024-09-13 17:31:31 +0900

    ツイート機能の作成

--- twitter/src/app/hooks/useHomeHook.ts
+++ twitter/src/app/hooks/useHomeHook.ts
@@ -1,11 +1,34 @@
+import { useForm } from "react-hook-form";
 import axiosInstance from "../lib/axiosInstance";
 import { TweetType } from "../types/tweet";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { tweetPatchSchema, tweetPatchSchemaType } from "../lib/validations/tweet";
+import { useEffect, useState } from "react";
 
-export default async function useHomeHook() {
+export default function useHomeHook() {
     const { instance } = axiosInstance();
+    const [tweets, setTweets] = useState<TweetType[]>([]);
 
-    const response = await instance.get('api/tweet');
-    const tweets: TweetType[] = response.data as TweetType[];
 
-    return { tweets };
+    const { register, handleSubmit, formState: { errors } } = useForm<tweetPatchSchemaType>({
+        resolver: zodResolver(tweetPatchSchema)
+    });
+
+    async function onSubmit(data: tweetPatchSchemaType) {
+        const response = await instance.post('api/tweet', data);
+        console.log(response);
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await instance.get('api/tweet');
+            if (response !== undefined) {
+                setTweets(response.data as TweetType[]);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    return { tweets, register, handleSubmit, errors, onSubmit };
 }
