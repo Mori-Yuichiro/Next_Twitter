629cdc5ed4e26d36b1aa01db5a43a663b6750556
森祐一朗 <apotikisin4632@gmail.com>
2024-09-14 14:41:00 +0900

    レスポンシブ対応　トップページ

--- twitter/src/app/components/Sidebar.tsx
+++ twitter/src/app/components/Sidebar.tsx
@@ -6,7 +6,7 @@ export default async function Sidebar() {
     const { ITEM_LIST, user } = await useSidebarHook();
 
     return (
-        <aside className="flex flex-col gap-3 h-screen w-60 border-r border-black p-4">
+        <aside className="flex flex-col gap-3 h-screen max-lg:w-20 w-60 border-r border-black p-4 fixed">
             <Link href={'/home'}>
                 <svg width="28" height="28" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="white" />
@@ -18,14 +18,14 @@ export default async function Sidebar() {
                         {item.disabled ?
                             <div className="flex items-center gap-5 hover:bg-slate-300">
                                 <div dangerouslySetInnerHTML={{ __html: item.svg }} />
-                                <h1 className="text-xl">{item.label}</h1>
+                                <h1 className="text-xl max-lg:hidden">{item.label}</h1>
                             </div> :
                             <Link
                                 href={item.href}
                                 className="flex items-center gap-5 hover:bg-slate-300"
                             >
                                 <div dangerouslySetInnerHTML={{ __html: item.svg }} />
-                                <h1 className="text-xl">{item.label}</h1>
+                                <h1 className="text-xl max-lg:hidden">{item.label}</h1>
                             </Link>
                         }
                     </div>
