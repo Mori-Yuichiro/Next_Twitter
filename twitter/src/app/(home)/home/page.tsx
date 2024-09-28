f042d1900dadd93e949163d0ae890b8ca486e23c
森祐一朗 <apotikisin4632@gmail.com>
2024-09-28 14:46:50 +0900

    ツイート投稿機能の作成

--- twitter/src/app/(home)/home/page.tsx
+++ twitter/src/app/(home)/home/page.tsx
@@ -4,8 +4,18 @@ import Button from "@/components/Button";
 import Tweet from "@/components/Tweet";
 import useHomeHook from "@/hooks/useHomeHook";
 
+
 export default function HomePage() {
-    const { tweets, register, handleSubmit, errors, onSubmit } = useHomeHook();
+    const { tweets,
+        register,
+        handleSubmit,
+        errors,
+        onSubmit,
+        inputRef,
+        fileUpload,
+        fileInput,
+        images
+    } = useHomeHook();
 
     return (
         <div>
@@ -19,31 +29,52 @@ export default function HomePage() {
             </ul>
             <div className="border-b border-black p-3">
                 {errors && <p className="text-red-500">{errors.content?.message}</p>}
-                <form
-                    onSubmit={handleSubmit(onSubmit)}
-                    className="flex flex-col gap-5"
-                >
-                    <input
-                        id="content"
-                        type="text"
-                        placeholder="What's happening?!"
-                        className="p-3 rounded-full w-2/3"
-                        {...register("content")}
-                    />
-                    <div className="flex justify-between">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 56 56"><path fill="currentColor" d="M7.715 49.574h40.57c4.899 0 7.36-2.437 7.36-7.265V13.69c0-4.828-2.461-7.265-7.36-7.265H7.715C2.84 6.426.355 8.84.355 13.69v28.62c0 4.851 2.485 7.265 7.36 7.265m31.57-21.633c-1.055-.937-2.25-1.43-3.515-1.43c-1.313 0-2.462.446-3.54 1.407l-10.593 9.469l-4.336-3.938c-.985-.867-2.04-1.336-3.164-1.336c-1.032 0-2.04.446-3 1.313L4.129 39.73V13.88c0-2.438 1.312-3.68 3.656-3.68h40.43c2.32 0 3.656 1.242 3.656 3.68v25.875Zm-21.469.258c3.024 0 5.508-2.484 5.508-5.531c0-3.023-2.484-5.531-5.508-5.531c-3.046 0-5.53 2.508-5.53 5.531a5.541 5.541 0 0 0 5.53 5.531" /></svg>
-                        <Button className="bg-cyan-400 rounded-full w-20 p-1">Post</Button>
-                    </div>
-                </form>
-            </div>
-            {tweets.map(tweet => (
-                <div
-                    key={tweet.id}
-                    className="border-b border-black"
-                >
-                    <Tweet tweet={tweet} />
+                <input
+                    id="content"
+                    type="text"
+                    placeholder="What's happening?!"
+                    className="p-3 rounded-full w-2/3"
+                    {...register("content")}
+                />
+                <div className="grid grid-cols-2 w-96 gap-x-5 my-5">
+                    {images &&
+                        images.map((image, i) => {
+                            return (
+                                <img
+                                    key={i}
+                                    src={image.data as string}
+                                    alt={image.fileName}
+                                />)
+                        })
+                    }
                 </div>
-            ))}
-        </div>
+                <div className="flex justify-between">
+                    <input
+                        type="file"
+                        multiple
+                        className="hidden"
+                        ref={inputRef}
+                        onChange={fileInput}
+                    />
+                    <button onClick={fileUpload}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 56 56"><path fill="currentColor" d="M7.715 49.574h40.57c4.899 0 7.36-2.437 7.36-7.265V13.69c0-4.828-2.461-7.265-7.36-7.265H7.715C2.84 6.426.355 8.84.355 13.69v28.62c0 4.851 2.485 7.265 7.36 7.265m31.57-21.633c-1.055-.937-2.25-1.43-3.515-1.43c-1.313 0-2.462.446-3.54 1.407l-10.593 9.469l-4.336-3.938c-.985-.867-2.04-1.336-3.164-1.336c-1.032 0-2.04.446-3 1.313L4.129 39.73V13.88c0-2.438 1.312-3.68 3.656-3.68h40.43c2.32 0 3.656 1.242 3.656 3.68v25.875Zm-21.469.258c3.024 0 5.508-2.484 5.508-5.531c0-3.023-2.484-5.531-5.508-5.531c-3.046 0-5.53 2.508-5.53 5.531a5.541 5.541 0 0 0 5.53 5.531" /></svg>
+                    </button>
+                    <Button
+                        className="bg-cyan-400 rounded-full w-20 p-1"
+                        onClick={handleSubmit(onSubmit)}
+                    >Post</Button>
+                </div>
+            </div>
+            {
+                tweets.map(tweet => (
+                    <div
+                        key={tweet.id}
+                        className="border-b border-black"
+                    >
+                        <Tweet tweet={tweet} />
+                    </div>
+                ))
+            }
+        </div >
     );
 }
--- twitter/src/hooks/useHomeHook.ts
+++ twitter/src/hooks/useHomeHook.ts
@@ -3,21 +3,69 @@ import axiosInstance from "../lib/axiosInstance";
 import { TweetType } from "../app/types/tweet";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { tweetPatchSchema, tweetPatchSchemaType } from "../lib/validations/tweet";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { fileRead } from "@/lib/fileUpload";
+import { ImageType } from "@/app/types/image";
+
 
 export default function useHomeHook() {
     const { instance } = axiosInstance();
     const [tweets, setTweets] = useState<TweetType[]>([]);
 
+    const [images, setImages] = useState<ImageType[]>([]);
+    const [imageDatas, setImageDatas] = useState<(string | ArrayBuffer | null)[]>([]);
+
+    const fileInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedImages = [];
+        const selectedImageDatas = [];
+
+        const files = Array.from(e.target.files || []);
+        for (const file of files) {
+            selectedImages.push({
+                data: await fileRead(file),
+                fileName: file.name
+            });
+            selectedImageDatas.push(await fileRead(file));
+            setImageDatas([...imageDatas, ...selectedImageDatas]);
+        }
+        setImages([...images, ...selectedImages]);
+    }
+
+    const uploadImage = async (imageData: string | ArrayBuffer | null) => {
+        return await instance.post('api/image-upload', {
+            imageData
+        });
+    }
 
     const { register, handleSubmit, formState: { errors } } = useForm<tweetPatchSchemaType>({
         resolver: zodResolver(tweetPatchSchema)
     });
 
     async function onSubmit(data: tweetPatchSchemaType) {
-        const response = await instance.post('api/tweet', data);
-        console.log(response);
-        window.location.reload();
+        try {
+            if (images.length > 0) {
+                const imageUrls: string[] = await Promise.all(imageDatas.map(async (imageData) => {
+                    const data = await uploadImage(imageData);
+                    return data.data;
+                }));
+                await instance.post('api/tweet', {
+                    ...data,
+                    imageUrls
+                });
+            } else {
+                await instance.post('api/tweet', data);
+            }
+            window.location.reload();
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const fileUpload = () => {
+        if (inputRef.current) {
+            inputRef.current.click();
+        }
     }
 
     useEffect(() => {
@@ -31,5 +79,16 @@ export default function useHomeHook() {
         fetchData();
     }, [])
 
-    return { tweets, register, handleSubmit, errors, onSubmit };
+    return {
+        tweets,
+        register,
+        handleSubmit,
+        errors,
+        onSubmit,
+        inputRef,
+        fileUpload,
+        fileInput,
+        images,
+        uploadImage
+    };
 }
